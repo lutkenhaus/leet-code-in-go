8c78dabb24da350f45501d0b09984a07d9eef890
lutkenhaus <gustavomayron9@gmail.com>
2025-07-16 22:52:55 -0300

    solved

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func fizzBuzz(n int) []string {
+	if n < 1 || n > int(math.Pow(10, 4)) {
+		return []string{}
+	}
+
+	result := make([]string, n)
+
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%15 == 0:
+			result[i-1] = "FizzBuzz"
+		case i%3 == 0:
+			result[i-1] = "Fizz"
+		case i%5 == 0:
+			result[i-1] = "Buzz"
+		default:
+			result[i-1] = fmt.Sprintf("%d", i)
+		}
+	}
+
+	return result
+}
 
 func main() {
-	fmt.Println("Leet Code!")
+	fmt.Println("[", strings.Join(fizzBuzz(3), ", "), "]")
+	fmt.Println("[", strings.Join(fizzBuzz(5), ", "), "]")
+	fmt.Println("[", strings.Join(fizzBuzz(15), ", "), "]")
 }
