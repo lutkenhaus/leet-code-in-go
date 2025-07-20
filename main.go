07d958746552612304454aba2acc285c90827b9c
lutkenhaus <gustavomayron9@gmail.com>
2025-07-20 12:25:20 -0300

    boilerplate code

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
+// Problem:
+// -
+
+// First thoughts:
+// -
+
+// Approach:
+// -
+
+// Key:
+// -
+
+// Constraints:
+// -
+
+// Optmizations:
+// -
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func someFunc(string) string {
+	return "LeetCode!"
+}
 
 func main() {
-	fmt.Println("Leet Code!")
+	testCases := []struct {
+		value         string
+		expectedValue string
+	}{
+		{"LeetCode!", "LeetCode!"},
+		{"LeetCode", "LeetCode!"},
+		{"leetcode", "LeetCode!"},
+	}
+
+	for _, tc := range testCases {
+		response := someFunc(tc.value)
+		if response != tc.expectedValue {
+			fmt.Printf("error: wanted %v, got (%v)\n", tc.expectedValue, response)
+			return
+		}
+		fmt.Printf("%v - %v\n", tc.value, response)
+	}
 }
