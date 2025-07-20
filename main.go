66bdba933e5e1fad55b1f385a6bf1ecf22102a34
lutkenhaus <gustavomayron9@gmail.com>
2025-07-20 15:01:45 -0300

    first solution

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 // Problem:
-// -
+// - Given an integer x, return true if x is a palindrome, and false otherwise.
 
 // First thoughts:
-// -
+// - Create a reversed copy of x.
+// - Compare the copy of x to x.
 
 // Approach:
-// -
+// - Transform the interger into an array on integers.
+// - Reverse the integer using a inverted loop.
+// - Changed approach to use math in order to invert the value.
+// - Kept two slices, one in original order, other one inverted.
+// - Compare two slices to check if x is a palindrome.
 
 // Key:
-// -
+// - Use math. Remainder of x divided by 10 is equal to last digit.
+// - remainder = x % 10
+// - Use two slices for comparison.
 
 // Constraints:
-// -
+// - ( -231 <= x <= 231 - 1 )
 
 // Optmizations:
 // -
@@ -22,26 +29,54 @@ import (
 	"fmt"
 )
 
-func someFunc(s string) string {
-	return s + "!"
+func isPalindrome(x int) bool {
+	if x == 0 {
+		return true
+	}
+
+	if x < 0 {
+		return false
+	}
+
+	var arrX []int
+	var arrInvertedX []int
+	currentDigit := 0
+
+	for x > 0 {
+		currentDigit = x % 10
+		arrInvertedX = append(arrInvertedX, currentDigit)
+		arrX = append([]int{currentDigit}, arrX...)
+		x = x / 10
+	}
+
+	for i := range arrX {
+		if arrX[i] != arrInvertedX[i] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
+	fmt.Println("9. Palindrome Number")
 	testCases := []struct {
-		value         string
-		expectedValue string
+		value         int
+		expectedValue bool
 	}{
-		{"LeetCode", "LeetCode!"},
-		{"LeetCode!", "LeetCode!!"},
-		{"LeetCode!!", "LeetCode!!!"},
+		{121, true},
+		{-121, false},
+		{10, false},
+		{542, false},
+		{777, true},
 	}
 
-	response := ""
-	for _, tc := range testCases {
-		response = someFunc(tc.value)
+	response := false
+	for i, tc := range testCases {
+		response = isPalindrome(tc.value)
 		if response != tc.expectedValue {
 			fmt.Printf("error: wanted (%v), got (%v)\n", tc.expectedValue, response)
 		}
-		fmt.Printf("%v - %v\n", tc.value, response)
+		fmt.Printf("PASSED for test case (%d): %v - %v\n", i, tc.value, response)
 	}
 }
