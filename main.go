357d4eeadda85852839b38cd578b97454d8b22f0
lutkenhaus <gustavomayron9@gmail.com>
2025-07-23 23:01:25 -0300

    started this challenge

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 // Problem:
-// -
+// - Given the heads of two sorted linked lists list1 and list2.
+// Merge the lists into one sorted list.
+// The list should be made by splicing together the nodes of the first two lists.
+// - Return the head of the merged linked list.
 
 // First thoughts:
 // -
@@ -25,8 +28,30 @@ import (
 	"fmt"
 )
 
-func someFunc(s string) string {
-	return s + "!"
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type LinkedList struct {
+	Head *ListNode
+}
+
+func (ll *LinkedList) Append(value int) {
+	// TODO: create a logic to append a value to the list.
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	linkedList := LinkedList{}
+	return linkedList.Head
 }
 
 func main() {
