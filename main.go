2d2af6886a9f9e238d9abd56fb1061462856bc6d
lutkenhaus <gustavomayron9@gmail.com>
2025-07-24 22:04:31 -0300

    solution using pointers (shouldn't be used like this)

--- main.go
+++ main.go
@@ -17,7 +17,9 @@
 // -
 
 // Constraints:
-// -
+// - The number of nodes in both lists is in the range [0, 50].
+// - -100 <= Node.val <= 100
+// - Both list1 and list2 are sorted in non-decreasing order.
 
 // Optimizations:
 // -
@@ -37,16 +39,47 @@ import (
  */
 
 type ListNode struct {
-	Val  int
+	Val  *int
 	Next *ListNode
+}
+
+func NewEmptyNode() *ListNode {
+	return &ListNode{
+		Val:  nil,
+		Next: nil,
+	}
+}
+
+func NewNode(val int) *ListNode {
+	return &ListNode{
+		Val:  &val,
+		Next: nil,
+	}
 }
 
 type LinkedList struct {
 	Head *ListNode
 }
 
-func (ll *LinkedList) Append(value int) {
-	newListNode := &ListNode{Val: value}
+func NewEmptyList() *LinkedList {
+	return &LinkedList{
+		Head: nil,
+	}
+}
+
+func NewLinkedList(node ListNode) *LinkedList {
+	return &LinkedList{
+		Head: &node,
+	}
+}
+
+func (ll *LinkedList) Append(value *int) {
+	if value == nil {
+		fmt.Printf("possible nil pointer dereference error")
+		return
+	}
+
+	newListNode := NewNode(*value)
 
 	if ll.Head == nil {
 		ll.Head = newListNode
@@ -60,32 +93,134 @@ func (ll *LinkedList) Append(value int) {
 	current.Next = newListNode
 }
 
-func (ll *LinkedList) Preppend(value int) {
-	// TODO: implement preppend logic
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+
+	mergedLinkedList := NewEmptyList()
+	var currentListNode1, currentListNode2 *ListNode
+	if list1 != nil {
+		if list1.Val != nil && list1.Next != nil {
+			currentListNode1 = &ListNode{Val: list1.Val, Next: list1.Next}
+		} else if list1.Val != nil {
+			currentListNode1 = &ListNode{Val: list1.Val}
+		}
+	}
+	if list2 != nil {
+		if list2.Val != nil && list2.Next != nil {
+			currentListNode2 = &ListNode{Val: list2.Val, Next: list2.Next}
+		} else if list2.Val != nil {
+			currentListNode2 = &ListNode{Val: list2.Val}
+		}
+	}
+
+	for currentListNode1 != nil || currentListNode2 != nil {
+		switch {
+		case currentListNode1 == nil:
+			mergedLinkedList.Append(currentListNode2.Val)
+			currentListNode2 = currentListNode2.Next
+		case currentListNode2 == nil:
+			mergedLinkedList.Append(currentListNode1.Val)
+			currentListNode1 = currentListNode1.Next
+		case *currentListNode1.Val > *currentListNode2.Val:
+			mergedLinkedList.Append(currentListNode2.Val)
+			currentListNode2 = currentListNode2.Next
+		case *currentListNode1.Val < *currentListNode2.Val:
+			mergedLinkedList.Append(currentListNode1.Val)
+			currentListNode1 = currentListNode1.Next
+		default:
+			mergedLinkedList.Append(currentListNode1.Val)
+			mergedLinkedList.Append(currentListNode2.Val)
+			currentListNode1 = currentListNode1.Next
+			currentListNode2 = currentListNode2.Next
+		}
+	}
+
+	fmt.Println("")
+	mergedLinkedList.PrintLinkedList()
+
+	return mergedLinkedList.Head
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	linkedList := LinkedList{}
-	return linkedList.Head
+func (ll *LinkedList) PrintLinkedList() {
+	current := ll.Head
+	for current != nil {
+		fmt.Printf("%d -> ", *current.Val)
+		current = current.Next
+	}
+	fmt.Println("nil")
 }
 
 func main() {
+
+	zero := 0
+	one := 1
+	two := 2
+	three := 3
+	four := 4
+
+	ll1 := NewEmptyList()
+	ll2 := NewEmptyList()
+	expectedValue1 := NewEmptyList()
+
+	ll1.Append(&one)
+	ll1.Append(&two)
+	ll1.Append(&four)
+
+	ll2.Append(&one)
+	ll2.Append(&three)
+	ll2.Append(&four)
+
+	expectedValue1.Append(&one)
+	expectedValue1.Append(&one)
+	expectedValue1.Append(&two)
+	expectedValue1.Append(&three)
+	expectedValue1.Append(&four)
+	expectedValue1.Append(&four)
+
+	ll3 := NewEmptyList()
+	ll4 := NewEmptyList()
+	expectedValue2 := NewEmptyNode()
+
+	ll5 := NewEmptyList()
+	ll6 := NewEmptyList()
+	expectedValue3 := NewEmptyList()
+
+	ll6.Append(&zero)
+	expectedValue3.Append(&zero)
+
 	testCases := []struct {
 		value1        *ListNode
 		value2        *ListNode
 		expectedValue *ListNode
 	}{
-		{&ListNode{}, &ListNode{}, &ListNode{}},
-		{&ListNode{}, &ListNode{}, &ListNode{}},
-		{&ListNode{}, &ListNode{}, &ListNode{}},
+		{ll1.Head, ll2.Head, expectedValue1.Head},
+		{ll3.Head, ll4.Head, expectedValue2},
+		{ll5.Head, ll6.Head, expectedValue3.Head},
 	}
 
 	response := &ListNode{}
 	for i, tc := range testCases {
+		currentMergedList := &ListNode{}
+		currentExpectedList := &ListNode{}
 		response = mergeTwoLists(tc.value1, tc.value2)
-		if response != tc.expectedValue {
-			fmt.Printf("error: wanted (%v), got (%v)\n", tc.expectedValue, response)
+
+		// TODO: implement better test cases check...
+		switch {
+		case tc.value1 == nil || tc.value2 == nil:
+			fmt.Printf("possible empty lists, use debugger to check the return value")
+		default:
+			currentMergedList = response
+			currentExpectedList = tc.expectedValue
+			for currentExpectedList != nil {
+				if *currentMergedList.Val != *currentExpectedList.Val {
+					fmt.Printf("error: wanted (%v), got (%v)\n", *currentMergedList.Val, *currentExpectedList.Val)
+				}
+				currentMergedList = currentMergedList.Next
+				currentExpectedList = currentExpectedList.Next
+			}
+			fmt.Printf("Testcase: %d - Passed!\n", i)
 		}
-		fmt.Printf("Test passed for testcase: %d - a + b = %v\n", i, response)
 	}
 }
